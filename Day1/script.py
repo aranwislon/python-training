8b77af7bf99afcfeb9d462da06a94582ef9ea29a
Aran Wislon <rumah.tobing17@gmail.com>
2021-08-02 13:47:53 +0700

    remove some code

--- Day1/script.py
+++ Day1/script.py
@@ -9,8 +9,5 @@ def greet(who_to_greet):
     greeting = 'Hello, {}'.format(who_to_greet)
     return greeting
 
-print(greet('World'))
-print(greet('Corey  '))
-
-r = requests.get('https://coreyms.com')
-print(r.status_code)
+name = input("Your name? ")
+print("Hello ,",name)
